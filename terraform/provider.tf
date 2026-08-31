d84f4b21d571bbe04dc6960c8349b1cea93592f8
Abbas_Ali <abbasmohd939@gmail.com>
2026-08-31 13:09:37 -0500

    Configure Terraform remote state with S3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,18 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
+required_providers {
+aws = {
+source = "hashicorp/aws"
+}
+}
+
+backend "s3" {
+bucket       = "devops-phase2-tfstate-890968193149"
+key          = "terraform/terraform.tfstate"
+region       = "us-east-1"
+use_lockfile = true
+}
 }
 
 provider "aws" {
-  region = "us-east-1"
+region = var.aws_region
 }
